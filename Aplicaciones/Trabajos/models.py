83e7a08fbe44dd948c9daa581b20d80ae31c1008
joaquinortiz1 <joaquin.ortiz.que@gmail.com>
2023-11-02 17:45:04 -0300

    cambios en la app

--- Aplicaciones/Trabajos/models.py
+++ Aplicaciones/Trabajos/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class SolicitudDeTrabajo(models.Model):
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    requisitos = models.TextField()
+    ubicacion = models.CharField(max_length=50)
+    sueldo = models.DecimalField(max_digits=10, decimal_places=2)
+    tipo_de_trabajo = models.CharField(max_length=20, choices=[("Tiempo completo", "Tiempo completo"), ("Medio tiempo", "Medio tiempo"), ("Por horas", "Por horas")])
+    tipo_de_contrato = models.CharField(max_length=20, choices=[("Indefinido", "Indefinido"), ("Temporal", "Temporal"), ("Freelance", "Freelance")])
+    area_de_trabajo = models.CharField(max_length=50)
+    fecha_de_publicacion = models.DateField(auto_now_add=True)
+    especialidades_buscadas = models.ManyToManyField("Especialidad")
+    trabajo_remoto = models.BooleanField()
+    fecha_limite = models.DateField()
