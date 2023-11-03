f2aaa00e5ff10338dbc06b7a4ecb2cb316fb287e
joaquinortiz1 <joaquin.ortiz.que@gmail.com>
2023-11-02 23:49:34 -0300

    cambios en la vista y el html

--- Aplicaciones/Trabajos/urls.py
+++ Aplicaciones/Trabajos/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path ('solicitudes/', views.form)
+    path('solicitudes/', views.form),
+    path('registrarTrabajo/', views.registrarTrabajo)
 ]
--- Aplicaciones/Trabajos/views.py
+++ Aplicaciones/Trabajos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import SolicitudDeTrabajo
 
 # Create your views here.
@@ -6,3 +6,19 @@ from .models import SolicitudDeTrabajo
 def form(request):
     trabajosListados = SolicitudDeTrabajo.objects.all()
     return render(request, "gestionTrabajos.html", {"trabajos": trabajosListados})
+
+def registrarTrabajo(request):
+    titulo=request.POST['txtTitulo']
+    descripcion=request.POST['txtDescripcion']
+    requisitos=request.POST['txtRequisitos']
+    ubicacion=request.POST['txtUbicacion']
+    sueldo=request.POST['txtSueldo']
+    tipo_de_trabajo=request.POST['selTipoTrabajo']
+    tipo_de_contrato=request.POST['selTipoContrato']
+    area_de_trabajo=request.POST['txtAreaTrabajo']
+    fecha_de_publicacion=request.POST['fecha_publicacion']
+    trabajo_remoto=request.POST['chkTrabajoRemoto']
+    fecha_limite=request.POST['fecha_limite']
+
+    nuevo_trabajo = SolicitudDeTrabajo.objects.create(titulo=titulo, descripcion=descripcion, requisitos=requisitos, ubicacion=ubicacion, sueldo=sueldo, tipo_de_trabajo=tipo_de_trabajo, tipo_de_contrato=tipo_de_contrato, area_de_trabajo=area_de_trabajo, fecha_de_publicacion=fecha_de_publicacion, trabajo_remoto=trabajo_remoto, fecha_limite=fecha_limite)
+    return redirect('/')
